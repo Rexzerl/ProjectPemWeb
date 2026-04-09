6d17cac9c19f8a4d8dda0357ad437b4b41e37ba4
Fachrisya Maula Ardhi <fachrisardhi@gmail.com>
2026-04-09 09:48:55 +0700

    ubah source database

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $host = "localhost:3308";
 $user = "root";
 $pass = ""; 
-$db   = "mentor_campus"; 
+$db   = "mentor_kampus"; 
 $conn = mysqli_connect($host, $user, $pass, $db, 3308);
 $db   = "mentor_kampus"; 
 $conn = mysqli_connect($host, $user, $pass, $db, 3306);
