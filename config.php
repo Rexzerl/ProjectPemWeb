bd39d295a2fa46d64527b2a1d040196285f5eddd
Fachrisya Maula Ardhi <fachrisardhi@gmail.com>
2026-04-13 15:51:29 +0700

    fix config

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-$host = "localhost:3306";
+$host = "localhost:3308";
 $user = "root";
 $pass = ""; 
 $db   = "mentor_kampus"; 
-$conn = mysqli_connect($host, $user, $pass, $db, 3306);
+$conn = mysqli_connect($host, $user, $pass, $db, 3308);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
